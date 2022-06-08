4625e9b2301e85cae20eb4607e3084a0501b2f3b
James-K-Knight <57244441+James-K-Knight@users.noreply.github.com>
2022-06-08 10:58:57 +0800

    consolidate into single script, fix logging

--- dtools.sh
+++ dtools.sh
@@ -4,8 +4,6 @@
 # https://stackoverflow.com/a/41139446
 # https://gist.github.com/hfossli/4368aa5a577742c3c9f9266ed214aa58
 
-ACTIVE_DIR="/opt/docker-active/"
-
 # Logging Level configuration works as follows:
 # DEBUG - Provides all logging output
 # INFO  - Provides all but debug messages
@@ -16,8 +14,13 @@ ACTIVE_DIR="/opt/docker-active/"
 #
 scriptLoggingLevel="DEBUG"
 
+C_Clear='\033[0m'
+C_Red='\033[0;31m'
 
 function usage() {
+  if [ -n "$1" ]; then
+    echo -e "${C_Red}Exited: $1${C_Clear}\n";
+  fi
   echo "Usage: $0 [-l log-level]"
   echo "  -l, --log-level   The Logging Level"
   echo "                    DEBUG - Provides all logging output"
@@ -26,16 +29,24 @@ function usage() {
   echo "                    ERROR - Provides all but debug, info and warn"
   echo "                    SEVERE and CRITICAL are also supported levels as extremes of ERROR"
   echo ""
+  echo " -p, --pull	    Pull active docker-compose containers"
+  echo " -r, --restart	    Restart Active socker-compose containers"
   echo "Example: $0 --log-level INFO"
   exit 1
 }
 
+Pull_Con=0
+Restart_Con=0
 
 # parse params
 while [[ "$#" > 0 ]]; do case $1 in
   -l|--log-level) scriptLoggingLevel="$2"; shift; shift;;
+  -p|--pull) Pull_Con=1; shift; shift;;
+  -r|--restart) Restart_Con=1; shift; shift;;
   *) usage "Unknown parameter passed: $1"; shift; shift;;
 esac; done
+
+if [ "${Pull_Con}" -eq 0 ] && [ "${Restart_Con}" -eq 0 ]; then usage "No Action flag set"; fi;
 
 function logThis() {
         dateTime=$(date --rfc-3339=seconds)
@@ -52,7 +63,7 @@ function logThis() {
                 exit 1
         fi
 
-        logMessage="${IN}"
+        logMessage=$(echo "${IN}" | xargs)
         logMessagePriority="${1}"
 
         declare -A logPriorities=([DEBUG]=0 [INFO]=1 [WARN]=2 [ERROR]=3 [SEVERE]=4 [CRITICAL]=5)
@@ -83,8 +94,22 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 
-compose_projects=$(sudo docker ps --filter "label=com.docker.compose.project" -q | xargs -I {} sudo docker container inspect {} --format '{{ index .Config.Labels "com.docker.compose.project.working_dir" }}' | uniq)
-for working_dir in ${compose_projects}; do
-	logThis "INFO" "Pulling ${working_dir}"
-	(cd "$working_dir" &&  docker-compose pull)
+Compose_Projects=$(sudo docker ps --filter "label=com.docker.compose.project" -q | xargs -I {} sudo docker container inspect {} --format '{{ index .Config.Labels "com.docker.compose.project.working_dir" }}' | uniq)
+
+Count=$(echo "${Compose_Projects}" | grep -c '^')
+
+logThis "DEBUG" "${Count} containers found"
+
+for working_dir in ${Compose_Projects}; do
+	if [ "${Pull_Con}" -eq 1 ]
+	then
+		logThis "INFO" "Pulling ${working_dir}"
+		(cd "$working_dir" && docker-compose pull) 2>&1 | logThis "INFO"
+	fi
+        if [ "${Restart_Con}" -eq 1 ]
+        then
+                logThis "INFO" "Restarting ${working_dir}"
+                (cd "$working_dir" &&  docker-compose down && docker-compose up -d) 2>&1 | logThis "INFO"
+        fi
+
 done
